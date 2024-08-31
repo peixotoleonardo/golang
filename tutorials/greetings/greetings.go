c4a1c33381c950ee71f439b061d174f97de3c1ff
Leonardo Peixoto <leonardo.peixoto@fortics.com.br>
2024-08-31 11:39:17 -0300

    feat: add Hellos function

--- tutorials/greetings/greetings.go
+++ tutorials/greetings/greetings.go
@@ -14,6 +14,22 @@ func Hello(name string) (string, error) {
 	return fmt.Sprintf(randomFormat(), name), nil
 }
 
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
 func randomFormat() string {
 	formats := []string{
 		"Hi, %v. Welcome!",
